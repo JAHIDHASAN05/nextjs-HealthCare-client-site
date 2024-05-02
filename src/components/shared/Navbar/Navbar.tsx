09fc5ecc0213e4e5a6167c55e1c590db03d385d8
JAHID HASAN <developerjahid@gmail.com>
2024-05-02 22:57:29 +0600

    Navbar added withCommon LayOut

---
+++ src/components/shared/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Stack py={2} direction={"row"} justifyContent="space-between" alignItems={'center'}>
+        <Typography variant="h5" component={Link} href="/" fontWeight={700}>
+          P
+          <Box component={"span"} color="primary.main">
+            H
+          </Box>{" "}
+          Health care
+        </Typography>
+        <Stack direction="row" justifyContent={"space-between"} gap={4}>
+          <Typography component={Link} href="/consultaion">Consultaion </Typography>
+          <Typography>Health Plans</Typography>
+          <Typography>Medicine</Typography>
+          <Typography>Diagonostic</Typography>
+          <Typography>NGOs</Typography>
+        </Stack>
+        <Button component={Link} href="/login">Login</Button>
+      </Stack>
+    </Container>
+  );
+};
+
+export default Navbar;

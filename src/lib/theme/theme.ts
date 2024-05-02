d538999b81734456444c2e6456b3c8ce8a71ee5f
JAHID HASAN <developerjahid@gmail.com>
2024-05-02 22:09:00 +0600

    Meterial ui setup

---
+++ src/lib/theme/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1586fd',
+    },
+    secondary: {
+      main: '#666f73',
+    },
+  },
+});

a3dc941223c63f73a9b2737dd93ccaa78e335219
JAHID HASAN <developerjahid@gmail.com>
2024-05-03 21:02:08 +0600

    Hero section Right part (Homepage)

--- src/components/ui/Homepage/HeroSection/HeroSection.tsx
+++ src/components/ui/Homepage/HeroSection/HeroSection.tsx
@@ -48,9 +48,8 @@ const HeroSection = () => {
           sx={{
             width: "50%",
             marginTop: "2rem",
-            fontSize :'18px'
+            fontSize: "18px",
           }}
-
           fontWeight={50}
         >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam id
@@ -58,17 +57,51 @@ const HeroSection = () => {
           necessitatibus. Lorem ipsum dolor sit, amet consectetur adipisicing
           elit. Nulla, voluptatibus!
         </Typography>
-        <Stack direction={'row'} gap={5} mt={4}  >
-            <Button>Make appointment</Button>
-            <Button variant="outlined">Contack us</Button>
+        <Stack direction={"row"} gap={5} mt={4}>
+          <Button>Make appointment</Button>
+          <Button variant="outlined">Contack us</Button>
         </Stack>
       </Box>
       <Box
         sx={{
-          border: "1px solid red",
+        //   border: "1px solid red",
+          display: "flex",
+          position: "relative",
+          margin :0,
+          gap:2
+
         }}
       >
-        rihgt
+        <Box
+          sx={{
+            position: "absolute", 
+            top:-40,
+           left :'35%'     
+          }}
+        >
+          <Image src={assets.svgs.arrow} alt="arrow" />
+        </Box>
+        <Box mt={4}>
+          <Image src={assets.images.doctor1}  width={250} height={250} alt="bught" />
+        </Box>
+        <Box>
+          <Image src={assets.images.doctor2} width={250} height={250} alt="bught" />
+        </Box>
+        <Box sx={{
+            position :'absolute',
+            top : "220px",
+           left : '140px'
+        }}>
+          <Image src={assets.images.doctor3} width={240} height={250} alt="bught" />
+        </Box>
+        <Box sx={{
+            position :'absolute',
+            bottom :0,
+            right :0,
+            zIndex : -1
+        }}>
+          <Image src={assets.images.stethoscope} width={200} height={200} alt="bught" />
+        </Box>
       </Box>
     </Container>
   );

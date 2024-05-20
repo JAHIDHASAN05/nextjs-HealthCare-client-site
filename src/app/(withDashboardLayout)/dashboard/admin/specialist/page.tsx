d9f46533a251cde355ce1b10a69f5ece9cfa44e9
JAHID HASAN <developerjahid@gmail.com>
2024-05-20 17:39:46 +0600

    speciality created succesfully

--- src/app/(withDashboardLayout)/dashboard/admin/specialist/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialist/page.tsx
@@ -1,22 +1,73 @@
 "use client";
 
 import Modal from "@/components/shared/Modal/modal";
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import SpecilistModal from "./SpecilistModal";
-import { useGetAllSpecilityQuery } from "@/redux/api/specialitiesApi";
+import { useDeleteSpecilityMutation, useGetAllSpecilityQuery } from "@/redux/api/specialitiesApi";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Image from "next/image";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { toast } from "sonner";
+
+
 
 const SpecialistPage = () => {
+
+
+
+  const [deleteSpecility]= useDeleteSpecilityMutation({})
+
+  const handleDelete = async (id: string) => {
+    console.log(id);
+    const res=  await deleteSpecility(id).unwrap()
+    console.log(res);
+
+    if(res?.id){
+      toast.success("Deleted succesfully")
+    }
+  };
   const columns: GridColDef[] = [
-    { field: "title", headerName: "Title", width: 100 },
-    { field: "icon", headerName: "Image", width: 130 },
- 
- 
+    { field: "title", headerName: "Title", width: 400 },
+    {
+      field: "icon",
+      headerName: "Image",
+      flex: 1,
+      renderCell({ row }) {
+        return (
+          <Box>
+            <Image
+              src={row.icon}
+              alt="specilitly.svg"
+              width={100 - 60}
+              height={100 - 60}
+            />
+          </Box>
+        );
+      },
+    },
+    {
+      field: "Action",
+      headerName: "Action",
+      headerAlign :'center',
+      align :'center',
+      flex: 1,
+      renderCell({ row }) {
+        return (
+          <IconButton onClick={() => handleDelete(row.id)} aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        );
+      },
+    },
   ];
-
-
-  
 
   const { data: specilityData, isLoading } = useGetAllSpecilityQuery({});
   console.log(specilityData);
@@ -45,12 +96,13 @@ const SpecialistPage = () => {
           showing all specility
         </Typography>
       </Box>
-     {!isLoading ?  <Box>
-        <DataGrid
-          rows={specilityData || []}
-          columns={columns}   
-        />
-      </Box> : <h1>Loading</h1>}
+      {!isLoading ? (
+        <Box>
+          <DataGrid rows={specilityData || []} columns={columns} />
+        </Box>
+      ) : (
+        <h1>Loading</h1>
+      )}
     </Box>
   );
 };
--- src/redux/api/specialitiesApi.tsx
+++ src/redux/api/specialitiesApi.tsx
@@ -24,53 +24,19 @@ const specialitiesApi = baseApi.injectEndpoints({
       },
       providesTags :[tagTypes.speciality]
     }),
+
+    deleteSpecility: build.mutation({
+      query: (id) => {
+        
+        return {
+          url: `/specialties/${id}`,
+          method: "DELETE",         
+        };
+      },
+      invalidatesTags : [tagTypes.speciality]
+    }),
   }),
 });
 
-export const { useCreateSpecilityMutation, useGetAllSpecilityQuery } =
+export const { useCreateSpecilityMutation, useGetAllSpecilityQuery,  useDeleteSpecilityMutation } =
   specialitiesApi;
-
-// import { modifyPayload } from '@/utils/modifyPayload';
-// import { baseApi } from './baseApi';
-
-// const specialitiesApi = baseApi.injectEndpoints({
-//   endpoints: (build) => ({
-//     createSpecility: build.mutation({
-//       query: (data) => {
-
-//         const formData = modifyPayload(data);
-//         return {
-//           url: '/specialties',
-//           method: 'POST',
-//           body: formData,
-//           // No need to set content type explicitly for FormData
-//         };
-//       },
-//     }),
-//   }),
-// });
-
-// export const { useCreateSpecilityMutation } = specialitiesApi;
-
-// import { baseApi } from './baseApi';
-
-// const specialitiesApi = baseApi.injectEndpoints({
-//   endpoints: (build) => ({
-//     createSpecility: build.mutation({
-//       query: (data) => {
-//         const formData = new FormData();
-//         for (const key in data) {
-//           formData.append(key, data[key]);
-//         }
-//         return {
-//           url: '/specialties',
-//           method: 'POST',
-//           body: formData,
-//           // No need to set content type explicitly for FormData
-//         };
-//       },
-//     }),
-//   }),
-// });
-
-// export const { useCreateSpecilityMutation } = specialitiesApi
